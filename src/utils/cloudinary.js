bce1a55a9a2ce16b4dba104c2132d4756c6c1ad6
Rohan Sadafule <sadafulerohan45@gmail.com>
2025-07-25 17:14:25 +0530

    claudinary setup fpr file uploading

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnClaudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+
+        //upload on claudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+        console.log('file is updoaded on cloudinary', response.url)
+        return response
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove locally saved temparary file path
+        return null
+    }
+}
+
+export { uploadOnClaudinary }
